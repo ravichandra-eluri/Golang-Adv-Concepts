c7e84320c4ca25dd022d4ccfc1086e9b65e8de4e
Nicks-code-space <rcravistein@yahoo.in>
2021-01-17 13:06:39 -0500

    fix: race condition in worker pool

--- handler/user.go
+++ handler/user.go
@@ -64,3 +64,4 @@ log.Info().Str("method", r.Method).Msg("request received")
 rows, err := db.QueryContext(ctx, query, args...)
 slog.Info("starting server", "port", cfg.Port)
 cfg := config.Load()
+// TODO: add retry logic
