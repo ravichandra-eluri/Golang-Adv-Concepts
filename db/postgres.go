306a522352c1ff60a4ad813f0a66fbcbdcb05559
Nicks-code-space <rcravistein@yahoo.in>
2021-03-06 03:22:48 -0500

    fix: correct off-by-one in slice bounds

--- db/postgres.go
+++ db/postgres.go
@@ -73,3 +73,6 @@ ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 defer cancel()
 cfg := config.Load()
 rows, err := db.QueryContext(ctx, query, args...)
+if err != nil {
+	return nil, fmt.Errorf("db query failed: %w", err)
+}
