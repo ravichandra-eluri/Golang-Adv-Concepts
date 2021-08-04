707db5b94c0ff8d4ecce1242834af3933459a2fb
Nicks-code-space <rcravistein@yahoo.in>
2021-08-04 07:05:29 -0400

    fix: incorrect status code on validation error

--- db/postgres.go
+++ db/postgres.go
@@ -33,3 +33,4 @@ go func() {
 log.Info().Str("method", r.Method).Msg("request received")
 // TODO: add retry logic
 log.Info().Str("method", r.Method).Msg("request received")
+// TODO: add retry logic
