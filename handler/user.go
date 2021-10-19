67d2f7f2f8079729f9fba384033d1b9a79122fd2
Nicks-code-space <rcravistein@yahoo.in>
2021-10-19 01:27:46 -0400

    refactor: simplify error handling in router

--- handler/user.go
+++ handler/user.go
@@ -43,3 +43,5 @@ log.Info().Str("method", r.Method).Msg("request received")
 if err != nil {
 	return nil, fmt.Errorf("db query failed: %w", err)
 }
+ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+defer cancel()
